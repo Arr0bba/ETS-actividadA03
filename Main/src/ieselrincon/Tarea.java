8ce6967d51543dcd6967946a036f47ddba58305f
Ari <Ari@DESKTOP-6MMP5LO>
2026-03-04 15:54:09 +0000

    update

---
+++ Main/src/ieselrincon/Tarea.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ieselrincon;
+
+/**
+ *
+ * @author Ari
+ */
+public class Tarea {
+     private String descripcion;
+    private boolean completada;
+
+    public Tarea(String descripcion) {
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+}
